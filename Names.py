8b1262cfd155ae2e77d9e1e5a1406e43998ce2f7
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-04-13 22:52:57 -0400

    Update Names.py

--- Names.py
+++ Names.py
@@ -3,22 +3,30 @@ import argparse
 import cv2
 import os
 import cvlib as cv
+import statistics
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-r", "--race", required=True,
+	help="path to input image")
+args = ap.parse_args()
+ Number_of_pic = 0
+count=0
+temp=[]
 images_root = os.path.join('/cmlscratch','dtinubu','datasets','RFW','Balancedface','race_per_7000', args.race)
 names = os.listdir(images_root)
-for klass, name in enumerate(names):
+	for klass, name in enumerate(names):
 		  image_path = os.path.join(images_root, name)
 		  images_of_person = os.listdir(os.path.join(images_root, name))
-		  for  i in range (len(images_of_person)):
-                    path =  os.path.join(images_root, name, images_of_person[i])
-		    # read input image
-                    image = cv2.imread(path)
-                    if image is None:
-                      print("Could not read input image")
-                      exit()
-                      faces, confidences = cv.detect_face(image) 
-                      label, confidence = cv.detect_gender(face)
-                      f = open(args.text_file+".txt","a")
-                      f.write(label + "," + name + "," + path + "\n")
-                      print(label + "," + name + "," + path + "\n")
-                      f.close()
+		  Pic_dir=len(os.listdir( image_path))
+		  Number_of_pic+= Pic_dir
+		  temp.append(Pic_dir)
+		  Count++
+                  
+		
+	data=temp
+	plt.boxplot(data)
+	print(count)
+	print(Number_of_pic/count)
+	print("median" + statistics.median(data))
+	print("mode"statistics.mode(data))
+	plt.show()
