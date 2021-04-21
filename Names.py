dd4beae008a8951818e45a560148312a280cebad
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-04-21 18:22:31 -0400

    Update Names.py

--- Names.py
+++ Names.py
@@ -12,20 +12,19 @@ number_of_pic = 0
 count=0
 
 Races =  ('Caucasian','Indian','Asian','African')
-	for race in zip(Races):
-		images_root = os.path.join('/cmlscratch','dtinubu','datasets','RFW','Balancedface','race_per_7000', race)
-		temp=[]
-		names = os.listdir(images_root)
-		for klass, name in enumerate(names):
-			image_path = os.path.join(images_root, name)
-			images_of_person = os.listdir(os.path.join(images_root, name))
-			pic_dir = len(os.listdir(image_path))
-			number_of_pic+= pic_dir
-			temp.append(pic_dir)
-			count= count + 1
-
-		data=temp		
-		data=np.sort(data)
-		plt.plot(data label = race)
+         for race in zip(Races):
+            images_root = os.path.join('/cmlscratch','dtinubu','datasets','RFW','Balancedface','race_per_7000', race)
+            temp=[]
+            names = os.listdir(images_root)
+            for klass, name in enumerate(names):
+                image_path = os.path.join(images_root, name)
+                images_of_person = os.listdir(os.path.join(images_root, name))
+                pic_dir = len(os.listdir(image_path))
+                number_of_pic+= pic_dir
+                temp.append(pic_dir)
+                count= count + 1
+            data=temp
+	    data=np.sort(data)
+            plt.plot(data, label = race)
 	
 plt.savefig(race + '_spread.png')
