04ed281880d48c8270b66574cfd72b4a64f4aeb9
Dtinubu <73240396+Dtinubu@users.noreply.github.com>
2021-04-21 18:18:51 -0400

    Update Names.py

--- Names.py
+++ Names.py
@@ -7,27 +7,25 @@ import cvlib as cv
 import statistics
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-r", "--race", required=True,
-	help="path to input image")
 args = ap.parse_args()
 number_of_pic = 0
 count=0
-temp=[]
-images_root = os.path.join('/cmlscratch','dtinubu','datasets','RFW','Balancedface','race_per_7000', args.race)
-names = os.listdir(images_root)
-for klass, name in enumerate(names):
-        image_path = os.path.join(images_root, name)
-        images_of_person = os.listdir(os.path.join(images_root, name))
-        pic_dir = len(os.listdir(image_path))
-        number_of_pic+= pic_dir
-        temp.append(pic_dir)
-        count= count + 1
-                  
-data=temp		
-data=np.sort(data)
-plt.plot(data)
-plt.savefig(args.race + '_spread.png')
-print("mean " + str(statistics.mean(data)))
-print("median " + str(statistics.median(data)))
-print("mode "+ str(statistics.mode(data)))
 
+Races =  ('Caucasian','Indian','Asian','African')
+	for race in zip(Races):
+		images_root = os.path.join('/cmlscratch','dtinubu','datasets','RFW','Balancedface','race_per_7000', race)
+		temp=[]
+		names = os.listdir(images_root)
+		for klass, name in enumerate(names):
+			image_path = os.path.join(images_root, name)
+			images_of_person = os.listdir(os.path.join(images_root, name))
+			pic_dir = len(os.listdir(image_path))
+			number_of_pic+= pic_dir
+			temp.append(pic_dir)
+			count= count + 1
+
+		data=temp		
+		data=np.sort(data)
+		plt.plot(data label = race)
+	
+plt.savefig(race + '_spread.png')
